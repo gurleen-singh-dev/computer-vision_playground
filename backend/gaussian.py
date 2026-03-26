d16b63b1903e6a244b79b4cb8261a75a650c09a9
Gurleen Singh <gurleensingh342@gmail.com>
2026-03-26 17:02:00 +0530

    Modified code to return save blurred image

--- backend/gaussian.py
+++ backend/gaussian.py
@@ -1,9 +1,15 @@
 import cv2
 
-image = cv2.imread(
-    r'f:\SideProject\computer-vision_playground\backend\uploads\arch.png')
-blurred_image = cv2.GaussianBlur(image, (19, 19), 0)
-cv2.imshow('Original Image', image)
-cv2.imshow('Blurred Image', blurred_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+def gaussian(path):
+    image = cv2.imread(path)
+    blurred_image = cv2.GaussianBlur(image, (19, 19), 0)
+    output_path = path.replace(".", "_blurred_image.")
+    cv2.imwrite(output_path, blurred_image)
+    # cv2.imshow('Original Image', image)
+    # cv2.imshow('Blurred Image', blurred_image)
+    # cv2.waitKey(0)
+
+    return output_path
+
+
+# im = gaussian(r"f:\SideProject\computer-vision_playground\uploads\arch.png")
