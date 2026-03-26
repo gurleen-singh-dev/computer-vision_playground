ddc28c5f277d36e77cc521a8523ebdd86c1ab225
Gurleen Singh <gurleensingh342@gmail.com>
2026-03-26 17:25:25 +0530

    Added filenotfound exception handling

--- backend/gaussian.py
+++ backend/gaussian.py
@@ -1,7 +1,11 @@
 import cv2
 
+
 def gaussian(path):
     image = cv2.imread(path)
+    if image is None:
+        raise FileNotFoundError(f"Could not load image: {path}")
+
     blurred_image = cv2.GaussianBlur(image, (19, 19), 0)
     output_path = path.replace(".", "_blurred_image.")
     cv2.imwrite(output_path, blurred_image)
@@ -12,4 +16,4 @@ def gaussian(path):
     return output_path
 
 
-# im = gaussian(r"f:\SideProject\computer-vision_playground\uploads\arch.png")
+# print(gaussian(r"f:\SideProject\computer-vision_playground\uploads\arch.png"))
