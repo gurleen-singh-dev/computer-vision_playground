18971e7225391860144d6918ca5955fb60380d03
Gurleen Singh <gurleensingh342@gmail.com>
2026-03-26 17:28:32 +0530

    imported edge, gray and gaussian modules

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
-from fastapi import FastAPI, UploadFile, File, Request
+from fastapi import FastAPI, UploadFile, File, Request, Form
 import shutil
 from pydantic import BaseModel
 from pathlib import Path
-from fastapi.responses import FileResponse, HTMLResponse 
+from fastapi.responses import FileResponse, HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+import os
+from backend.edge import edges
+from backend.gaussian import gaussian
+from backend.grayscale import gray
+
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="frontend"),name="static")
+
+app.mount("/static", StaticFiles(directory="frontend"), name="static")
 templates = Jinja2Templates(directory='frontend')
-@app.get("/", response_class= HTMLResponse)
-async def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+
+UPLOAD_FOLDER = "uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+# @app.get("/", response_class=HTMLResponse)
+# def home(request: Request):
+#     return templates.TemplateResponse("index.html", {"request": request})
+
+# @app.post("/upload", response_class=HTMLResponse)
+# async def upload_image(request: Request,
+#                        file: UploadFile = File(...),
+#                        operation: str = Form(...)):
+#     file_path = f"{UPLOAD_FOLDER}/{file.filename}"
+
+#     with open(file_path, "wb") as buffer:
+#         shutil.copyfileobj(file.file, buffer)
+
+#     if operation == "grayscale":
+#         output_path = convert_to_grayscale(file_path)
+#     elif operation == "edge":
+#         output_path = detect_edges(file_path)
+#     else:
+#         output_path = file_path
+
+#     return templates.TemplateResponse("index.html", {
+#         "request": request,
+#         "output_image": output_path
+#     })
 
 # @app.post("/upload-image")
 # def upload_image(file: UploadFile = File(...)):
