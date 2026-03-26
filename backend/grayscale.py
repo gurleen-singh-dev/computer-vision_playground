db69d3ddfc645903e76e4cdb6e70a60eaec50708
Gurleen Singh <gurleensingh342@gmail.com>
2026-03-26 17:26:13 +0530

    modified code to return save grayscale image

--- backend/grayscale.py
+++ backend/grayscale.py
@@ -1,11 +1,19 @@
 import cv2
 
-image = cv2.imread(
-    r'f:\SideProject\computer-vision_playground\backend\uploads\arch.png', 0)
 
-if image is None:
-    print("Error: Could not read the image file")
-else:
-    cv2.imshow('Image', image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+def gray(path):
+    image = cv2.imread(path, 0)
+    if image is None:
+        raise FileNotFoundError(f"Could not load image: {path}")
+
+    output_path = path.replace(".", "_grayscaled_image.")
+    cv2.imwrite(output_path, image)
+
+    # cv2.imshow('Image', image)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+    return output_path
+
+
+# print(gray(r'uploads\castle.png'))
