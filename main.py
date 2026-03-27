c7bc305f5695777e606291201bee470470b293c6
Gurleen Singh <gurleensingh342@gmail.com>
2026-03-27 20:59:42 +0530

    exposed uploads folder to fastapi

--- main.py
+++ main.py
@@ -15,33 +15,40 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="frontend"), name="static")
 templates = Jinja2Templates(directory='frontend')
 
+app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
+
 UPLOAD_FOLDER = "uploads"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
-# @app.get("/", response_class=HTMLResponse)
-# def home(request: Request):
-#     return templates.TemplateResponse("index.html", {"request": request})
 
-# @app.post("/upload", response_class=HTMLResponse)
-# async def upload_image(request: Request,
-#                        file: UploadFile = File(...),
-#                        operation: str = Form(...)):
-#     file_path = f"{UPLOAD_FOLDER}/{file.filename}"
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
-#     with open(file_path, "wb") as buffer:
-#         shutil.copyfileobj(file.file, buffer)
 
-#     if operation == "grayscale":
-#         output_path = convert_to_grayscale(file_path)
-#     elif operation == "edge":
-#         output_path = detect_edges(file_path)
-#     else:
-#         output_path = file_path
+@app.post("/upload", response_class=HTMLResponse)
+async def upload_image(request: Request,
+                       file: UploadFile = File(...),
+                       operation: str = Form(...)):
+    file_path = f"{UPLOAD_FOLDER}/{file.filename}"
 
-#     return templates.TemplateResponse("index.html", {
-#         "request": request,
-#         "output_image": output_path
-#     })
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    if operation == "grayscale":
+        output_path = gray(file_path)
+    elif operation == "edge":
+        output_path = edges(file_path)
+    elif operation == "gaussian":
+        output_path = gaussian(file_path)
+    else:
+        output_path = file_path
+
+    return templates.TemplateResponse("index.html", {
+        "request": request,
+        "output_image": output_path
+    })
+
 
 # @app.post("/upload-image")
 # def upload_image(file: UploadFile = File(...)):
